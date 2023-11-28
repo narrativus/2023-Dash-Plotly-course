692dcde79441ccdddb0589f98001f0fa47a041d5
Alejandro Berrizbeitia <narrativus@proton.me>
2023-11-29 00:04:54 +0100

    Start tutorial section

---
+++ 2023-11-28_pre-work-belatedly/A Minimal Dash App/app.py
@@ -0,0 +1,29 @@
+# Example from: https://dash.plotly.com/minimal-app
+# To run the app, copy the above code into a new file named app.py
+# and type into your terminal the command: `$poetry run python app.py`.
+# Then, go to the http link: http://127.0.0.1:8050/
+
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+if __name__ == '__main__':
+    app.run(debug=True)
